2165543311a30379de631e205783f1483fc3328f
Laplace <405420687@qq.com>
2024-03-07 01:25:07 +0800

    Update cs2rtv.cs

--- cs2rtv/cs2rtv.cs
+++ cs2rtv/cs2rtv.cs
@@ -124,7 +124,7 @@ public class Cs2rtv : BasePlugin
         {
             command.ReplyToCommand($"地图 {mapname} 不存在,地图列表已输出到控制台");
             foreach(string map in maplist)
-                cCSPlayer!.PrintToConsole($"map");
+                cCSPlayer!.PrintToConsole($"{map}");
         }
     }
 
